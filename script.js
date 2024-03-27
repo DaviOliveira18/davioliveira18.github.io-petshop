4b1c0b0605fa39616161ec03ec0bcac69341b9ee
DaviOliveira18 <134347042+DaviOliveira18@users.noreply.github.com>
2024-03-27 11:14:37 -0300

    v.2.0 alterações na página no geral(organização)
    fiz algmuas alterações necessárias mais ainda falta melhorar

---
+++ script.js
@@ -0,0 +1,29 @@
+function addToCart(event) {
+    const product = event.target.parentNode;
+    const productName = product.querySelector('h3').innerText;
+    const productImage = product.querySelector('img').src;
+    const productPrice = product.querySelector('p:nth-child(3)').innerText;
+  
+    // Cria um objeto para representar o produto
+    const productData = {
+      name: productName,
+      image: productImage,
+      price: productPrice
+    };
+  
+    // Obtém o carrinho atual do armazenamento local ou cria um novo array vazio
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+  
+    // Adiciona o produto ao carrinho
+    cart.push(productData);
+  
+    // Salva o carrinho atualizado de volta no armazenamento local
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+  
+  const buyButtons = document.querySelectorAll('.btnComprar');
+  
+  buyButtons.forEach(button => {
+    button.addEventListener('click', addToCart);
+  });
+  
